bb70687bf7e983a64446bddfde8a779662db690b
Diego Rojas <diego.rojas@edunext.co>
2017-12-14 12:11:26 -0500

    Fix PEP8, url encode and json responses

--- crm_integration_xblock/crm_integration_xblock.py
+++ crm_integration_xblock/crm_integration_xblock.py
@@ -4,25 +4,20 @@ CRM Integration Xblock.
 This module is the actual implemetation of the Xblock related classes
 """
 
-import pkg_resources
-import requests
 import json
 
-from django.views.decorators.csrf import csrf_exempt
-from django.utils.decorators import method_decorator
-from django.middleware.csrf import CsrfViewMiddleware, get_token
+from collections import OrderedDict
+from urllib import urlencode
+
+import requests
+import pkg_resources
 
 from xblock.core import XBlock
-from xblock.fields import Scope, String, Integer
+from xblock.fields import Scope, String
 from xblock.fragment import Fragment
 
 from xblockutils.studio_editable import StudioEditableXBlockMixin
 
-
-def load(path):
-    """Handy helper for getting resources from our kit."""
-    data = pkg_resources.resource_string(__name__, path)
-    return data.decode("utf8")
 
 class CrmIntegration(StudioEditableXBlockMixin, XBlock):
     """
@@ -37,40 +32,33 @@ class CrmIntegration(StudioEditableXBlockMixin, XBlock):
         default="Crm Integration"
     )
 
-    url = String(help="The URL for sandbox is "
-        "https://test.salesforce.com/services/oauth2/token and for production"
-        "https://login.salesforce.com/services/oauth2/token",
-        scope=Scope.content,
-        display_name="URL",
-        required = True
-    )
+    url = String(help="The URL for sandbox is https://test.salesforce.com/services/oauth2/token "
+                 "and for production https://login.salesforce.com/services/oauth2/token",
+                 scope=Scope.content,
+                 display_name="URL")
 
     client_id = String(help="",
-        scope=Scope.content,
-        display_name="Customer Key"
-    )
+                       scope=Scope.content,
+                       display_name="Customer Key")
 
-    client_secret = String(help="Your Customer Secret is found in the app you created in SalesForce"
-        "for Oauth. See instructions here: "
-        "https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/quickstart_oauth.htm",
-        scope=Scope.content,
-        display_name="Customer Secret"
-    )
+    client_secret = String(help="Your Customer Secret is found in the app you created in SalesForce "
+                           "for Oauth. See instructions here: "
+                           "https://developer.salesforce.com/docs/atlas.en-us.api_rest.meta/api_rest/"
+                           "quickstart_oauth.htm",
+                           scope=Scope.content,
+                           display_name="Customer Secret")
 
     username = String(help="Your Salesforce's email",
-        scope=Scope.content,
-        display_name="Email"
-    )
+                      scope=Scope.content,
+                      display_name="Email")
 
-    password = String(help="",
-        scope=Scope.content,
-        display_name="Password"
-    )
+    password = String(help="Your SalesForce's password",
+                      scope=Scope.content,
+                      display_name="Password")
 
     security_token = String(help="Be aware that your security token will change if you change your password",
-        scope=Scope.content,
-        display_name="Security Token"
-    )
+                            scope=Scope.content,
+                            display_name="Security Token")
 
     editable_fields = ('url',
                        'client_id',
@@ -118,20 +106,17 @@ class CrmIntegration(StudioEditableXBlockMixin, XBlock):
 
         return frag
 
-    def generate_token(self, url, client_id, client_secret, username, password, security_token):
+    def generate_token(self, url, client_id, client_secret, username, password, security_token):  # pylint: disable=too-many-arguments
         """
         This method generate an authentication token for SalesForce
         """
         # pylint: disable=unused-argument
 
-        payload = "grant_type=password&client_id={}&client_secret={}&username={}&password={}{}".format(
-            client_id,
-            client_secret,
-            username,
-            password,
-            security_token
-        )
-          
+        payload = urlencode(OrderedDict(grant_type="password", client_id=client_id,
+                                        client_secret=client_secret,
+                                        username=username,
+                                        password=password+security_token))
+
         headers = {'content-type': "application/x-www-form-urlencoded",}
         response = requests.request("POST", url, data=payload, headers=headers)
 
@@ -149,16 +134,15 @@ class CrmIntegration(StudioEditableXBlockMixin, XBlock):
         username = self.username
         password = self.password
         security_token = self.security_token
-        
-        token = self.generate_token(url, client_id, client_secret, username, password, security_token)
 
+        token = self.generate_token(url, client_id, client_secret, username, password, security_token)
         if token.status_code != 200:
-            return {"status": 0, "message": "Token not generated"}
+            return {"status_code": token.status_code, "message": "Token not generated"}
         else:
             response_salesforce = json.loads(token.text)
             token = response_salesforce["access_token"]
             instance_url = response_salesforce["instance_url"]
-            
+
             # TODO: Here we need get the name of objetc too
             url = "{}/services/data/v41.0/sobjects/Proyectos__c/".format(instance_url)
 
@@ -171,10 +155,13 @@ class CrmIntegration(StudioEditableXBlockMixin, XBlock):
                 }
 
             response = requests.request("POST", url, data=payload, headers=headers)
-            return {"status": 1}
+
+            return {"status_code": response.status_code}
 
     def get_general_rendering_context(self, context=None):
-        # This method creates or adds to the generic context for rendering the html
+        """
+        This method creates or adds to the generic context for rendering the html
+        """
         if not context:
             context = {}
 
