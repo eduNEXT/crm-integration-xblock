e43ba8486d897546a0b309bc1e901760c091f948
Felipe Montoya <felipe.montoya@edunext.co>
2018-08-16 17:50:17 -0500

    Better override of configuration_helpers

--- crm_integration_xblock/tracking.py
+++ crm_integration_xblock/tracking.py
@@ -1,13 +1,14 @@
 """
 """
 import logging
+from django.conf import settings
 
 log = logging.getLogger(__name__)
 
 try:
-    from openedx.conf import settings
+    from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 except Exception as e:
-    from django.conf import settings
+    configuration_helpers = False
 
 try:
     from eventtracking import tracker
@@ -17,8 +18,10 @@ except Exception as e:
 
 def emit(event, priority=20, data=None):
     """Wrapper for the openedx tracker function"""
-
-    if priority > getattr(settings, "CRM_INTEGRATION_TRACKING_LEVEL", 20):
+    level = 30
+    if configuration_helpers:
+        level = configuration_helpers.get_value("CRM_INTEGRATION_TRACKING_LEVEL", level)
+    if priority > getattr(settings, "CRM_INTEGRATION_TRACKING_LEVEL", level):
         tracker.emit(
             event,
             data
